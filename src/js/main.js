7edd35f7580df4a6422074e1ab4b6a3cd6252159
gpyshenko <gpyshenko@gmail.com>
2018-01-25 22:40:07 +0500

    Добавлен Datepicker.

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import 'wallop/css/wallop.css'
 import 'wallop/css/wallop--fade.css'
 import 'wallop/css/wallop--slide.css'
 
+import 'pikaday/css/pikaday.css'
 // Импорт слайдера
 import Wallop from 'wallop';
 // Импорт скриптов слайдеров на сайте
@@ -17,6 +18,26 @@ menuNav.navMenu();
 
 import acc from './accardeon';
 acc.accardeon();
+
+import Pikaday from 'pikaday'
+let inputPicker = document.querySelectorAll('.datepicker');
+let pikaSingle = document.querySelectorAll('.pika-single');
+[].forEach.call(inputPicker, function (e) {
+    let picker = new Pikaday({
+        field: e,
+        position: 'bottom right',
+
+    });
+    window.addEventListener("resize", function () {
+        picker.hide()
+    });
+});
+
+
+
+
+
+
 
 
 
